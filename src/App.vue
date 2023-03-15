922287c902d64c1ea3d69df5078fd1f22fa949b5
谭梦辉 <tanmenghui@shengzetianxia.com>
2023-03-15 09:22:01 +0800

    fix(): 重构布局切换

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Tansir
+ * @Date: 2023-02-22 10:53:52
+ * @LastEditors: Tansir
+ * @LastEditTime: 2023-03-15 09:16:21
+-->
 <template>
-  <el-config-provider :locale="lang">
+  <el-config-provider :locale="lang" :size="size">
     <router-view />
   </el-config-provider>
 </template>
@@ -10,7 +18,7 @@ import type { Language } from 'element-plus/es/locale/index';
 import zhCn from 'element-plus/dist/locale/zh-cn';
 import en from 'element-plus/dist/locale/en';
 import { useAppStore } from './stores/modules/app';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const appStore = useAppStore();
 const language: Record<string, Language> = {
@@ -21,4 +29,6 @@ const lang = ref(language[appStore.language]);
 appStore.$subscribe((mutation, state) => {
   lang.value = language[state.language];
 });
+
+const size = computed(() => appStore.size);
 </script>
--- src/components/size-select/index.vue
+++ src/components/size-select/index.vue
@@ -4,23 +4,23 @@
  * @Autor: Tansir
  * @Date: 2023-02-10 13:39:55
  * @LastEditors: Tansir
- * @LastEditTime: 2023-03-03 15:39:27
+ * @LastEditTime: 2023-03-15 09:17:15
 -->
 <template>
   <div>
     <el-dropdown id="size-select" trigger="click" @command="handleSetSize">
       <div>
-        <svg class="icon" aria-hidden="true" font-size="18px">
-          <use xlink:href="#icon-layout" />
+        <svg class="icon" aria-hidden="true" font-size="20px">
+          <use xlink:href="#icon-baocunbuju" />
         </svg>
       </div>
       <template #dropdown>
         <el-dropdown-menu>
           <el-dropdown-item
-            v-for="item of sizeOptions"
+            v-for="item of state.sizeOptions"
             :key="item.value"
-            :command="item.value"
             :disabled="size === item.value"
+            :command="item.value"
           >
             {{ item.label }}
           </el-dropdown-item>
@@ -31,48 +31,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { useTagsViewStateStore } from '@/stores/modules/tagsview';
+<script lang="ts" setup>
 import { useAppStore, type sizeType } from '@/stores/modules/app';
-import { defineComponent, reactive, nextTick, toRefs, computed } from 'vue';
+import { reactive, computed } from 'vue';
 import { ElMessage } from 'element-plus';
-// import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-  setup() {
-    // const route = useRoute();
-    // const router = useRouter();
-    function refreshView() {
-      useTagsViewStateStore().delAllCachedViews;
-      // const { fullPath } = route;
+const state = reactive({
+  sizeOptions: [
+    { label: 'Default', value: 'default' },
+    { label: 'Large', value: 'large' },
+    { label: 'Small', value: 'small' },
+  ],
+});
 
-      nextTick(() => {
-        // router.replace({
-        //   path: '/redirect' + fullPath,
-        // });
-        window.location.reload();
-      });
-    }
-    const state = reactive({
-      sizeOptions: [
-        { label: 'Default', value: 'default' },
-        { label: 'Large', value: 'large' },
-        { label: 'Small', value: 'small' },
-      ],
-      handleSetSize: (size: sizeType) => {
-        useAppStore().setSize(size);
-        refreshView();
-        ElMessage.success('切换大小成功');
-      },
-    });
-    const size = computed(() => {
-      return useAppStore().size;
-    });
-    return {
-      ...toRefs(state),
-      size,
-    };
-  },
+const handleSetSize = (size: sizeType) => {
+  useAppStore().setSize(size);
+  ElMessage.success('Switch Size Success');
+};
+
+const size = computed(() => {
+  return useAppStore().size;
 });
 </script>
 <style lang="scss" scoped>
